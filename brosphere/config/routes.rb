1e731e245fb4cb32d42d9f7540b397d260761d18
Alexandre Careil <alex.careil.fr@gmail.com>
2016-05-19 23:27:20 +0200

    add root static page

--- brosphere/config/routes.rb
+++ brosphere/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'brhome'
+  root 'static_pages#brhome'
 
-  get 'brhelp'
+  get 'static_pages/brhelp'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
