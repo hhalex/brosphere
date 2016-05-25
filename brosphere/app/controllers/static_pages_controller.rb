5fa7b87a0c58636e87b6d16cce0f8d7fc26d6647
Alexandre Careil <a.careil@criteo.com>
2016-05-25 14:09:32 +0200

    Add a brabout static page

--- brosphere/app/controllers/static_pages_controller.rb
+++ brosphere/app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def brhelp
   end
+
+  def brabout
+  end
 end
--- brosphere/config/routes.rb
+++ brosphere/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#brhome'
 
   get 'static_pages/brhelp'
+  get 'static_pages/brabout'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
