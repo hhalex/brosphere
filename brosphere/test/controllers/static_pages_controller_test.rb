b73fc765fae43fe11529c893faf9f7e76267f1fc
Alexandre Careil <alex.careil.fr@gmail.com>
2016-05-31 16:16:15 +0200

    Add dynamic titles to static pages

--- brosphere/test/controllers/static_pages_controller_test.rb
+++ brosphere/test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get brhome" do
     get :brhome
     assert_response :success
+    assert_select "title", "BrHome"
   end
 
   test "should get brhelp" do
     get :brhelp
     assert_response :success
+    assert_select "title", "BrHelp"
   end
 
   test "should get brabout" do
     get :brabout
     assert_response :success
+    assert_select "title", "AboutBros"
   end
 end
