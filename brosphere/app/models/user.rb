eb455c4632272d228b55e084a5ed37a070c39066
Alexandre Careil <alex.careil.fr@gmail.com>
2016-06-30 17:41:17 +0200

    Add format validation and uniqueness for mails

--- brosphere/app/models/user.rb
+++ brosphere/app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessor :name, :email
+  # \A is equivalent to ^
+  # \z is equivalent to $
+  # \d is the digit class
+  # \w is the word character class
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence:true, length: {maximum: 50}
-  validates :email, presence:true, length: {maximum: 255}
+  validates :email, presence:true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
   
 end
--- brosphere/db/schema.rb
+++ brosphere/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629141814) do
+ActiveRecord::Schema.define(version: 20160630153733) do
 
   create_table "fermentables", force: :cascade do |t|
     t.string   "name"
@@ -65,5 +65,7 @@ ActiveRecord::Schema.define(version: 20160629141814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
