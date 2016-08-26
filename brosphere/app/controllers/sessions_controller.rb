80f059c62edf49c59dbf059e2998c9e25c5d1d79
Alexandre Careil <alex.careil.fr@gmail.com>
2016-08-26 17:45:45 +0200

    Automatic persistent login
    Change-Id: I9cbfe74316fa58cf2e715936979a51fa6faa0183

--- brosphere/app/controllers/sessions_controller.rb
+++ brosphere/app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       flash.now[:success] = "Cool bro " + user.name + "! You're in the brosphere."
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = "Authentication problem"
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- brosphere/app/models/user.rb
+++ brosphere/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  #attr_accessor :name, :email
+  # Creates an attribute not present in the database
+  # This one is the token stored in the cookies
+  attr_accessor :remember_token
+  
   before_save { self.email.downcase! }
   # \A is equivalent to ^
   # \z is equivalent to $
@@ -13,11 +16,35 @@ class User < ActiveRecord::Base
 
   validates :password, presence:true, length: {minimum:6}
   has_secure_password
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
   
-  # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  # Static methods
+  class << self
+    # Returns the hash digest of the given string.
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # returns a random token for remember
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 end
